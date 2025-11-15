7de27d73db8ece45c16177815b2d8618aa8d8ed2
andreimirel <andrei_mirel.birtia@stud.acs.upb.ro>
2025-11-15 18:47:45 +0200

    setup files for project

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    // Plugin-ul de aplicatie Android
+    id("com.android.application") version "8.4.2" apply false
+    // Plugin-ul Kotlin pentru Android
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    // Plugin-ul KSP pentru Room
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
+    // Plugin-ul Google Services pentru Firebase
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
